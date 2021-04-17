d8457ff6a29215e43f46407a435703b6e2871db4
KamilSkrzynski <kamilskrzynski95@gmail.com>
2021-04-17 16:44:29 +0200

    Initial commit

---
+++ Items/ItemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItemsApp.swift
+//  Items
+//
+//  Created by Kamil Skrzyński on 17/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct ItemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
