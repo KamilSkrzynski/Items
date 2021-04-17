93599154306f5311ad1a8cb2fcee4f826e087ff7
KamilSkrzynski <kamilskrzynski95@gmail.com>
2021-04-17 16:48:15 +0200

    TabContainerView

---
+++ Items/Views/TabContainerView/TabContainerView.swift
@@ -0,0 +1,97 @@
+//
+//  TabContainerView.swift
+//  Items
+//
+//  Created by Kamil Skrzyński on 17/04/2021.
+//
+
+import SwiftUI
+
+struct TabContainerView: View {
+    
+    @State var selectedTab: Int = 0
+    @State var showAddSheet: Bool = false
+
+    var body: some View {
+        
+        VStack {
+            if selectedTab == 0 {
+                NavigationView {
+                    CollectionsView()
+                }
+            }
+            else if selectedTab == 1 {
+                NavigationView {
+                    ProfileView()
+                }
+            }
+            HStack {
+                Button(action: {
+                    self.selectedTab = 0
+                }, label: {
+                    Image(systemName: "square.grid.2x2.fill")
+                        .offset(x: 10, y: -5)
+                        .font(.system(size: 30))
+                        .foregroundColor(Color.primary.opacity(selectedTab == 0 ? 1.0 : 0.3))
+                })
+                
+                Spacer(minLength: 0)
+                
+                Button(action: {
+                    self.showAddSheet.toggle()
+                }, label: {
+                    ZStack {
+                        Circle()
+                            //      .stroke(Color.black)
+                            .foregroundColor(Color.secondaryColor)
+                            .frame(width: 80, height: 80)
+                            .shadow(radius: 20)
+                        
+                        Circle()
+                            .foregroundColor(Color.mainColor)
+                            .frame(width: 73, height: 73)
+                        
+                        Image(systemName: "plus")
+                            .font(.system(size: 25))
+                            .foregroundColor(.primary)
+                    }
+                    .offset(y: -30)
+                })
+                
+                Spacer(minLength: 0)
+                
+                Button(action: {
+                    self.selectedTab = 1
+                    
+                }, label: {
+                    Image(systemName: "person.fill")
+                        .offset(x: -10, y: -5)
+                        .font(.system(size: 30))
+                        .foregroundColor(Color.primary.opacity(selectedTab == 1 ? 1.0 : 0.3))
+                })
+            }
+            .frame(maxWidth:. infinity)
+            .padding(.horizontal, 50)
+            .frame(height: 100)
+            .background(Color.grayColor)
+        }
+        .accentColor(.primary)
+        .edgesIgnoringSafeArea(.all)
+        .fullScreenCover(isPresented: $showAddSheet) {
+            NavigationView {
+                NewItemView()
+            }
+        }
+    }
+}
+
+struct TabContainerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabContainerView()
+            .preferredColorScheme(.dark)
+        
+        
+        TabContainerView()
+            .preferredColorScheme(.light)
+    }
+}
