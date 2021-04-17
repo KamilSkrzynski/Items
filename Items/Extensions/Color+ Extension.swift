69c2873b3c24b239b996373511def176657c3b1a
KamilSkrzynski <kamilskrzynski95@gmail.com>
2021-04-17 17:09:55 +0200

    Colors extension

---
+++ Items/Extensions/Color+ Extension.swift
@@ -0,0 +1,20 @@
+//
+//  Color+ Extension.swift
+//  Items
+//
+//  Created by Kamil Skrzyński on 17/04/2021.
+//
+
+import SwiftUI
+
+struct Color__Extension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Color__Extension_Previews: PreviewProvider {
+    static var previews: some View {
+        Color__Extension()
+    }
+}
---
+++ Items/Extensions/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  Items
+//
+//  Created by Kamil Skrzyński on 17/04/2021.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let grayColor = Color("grayColor")
+    static let mainColor = Color("mainColor")
+    static let secondaryColor = Color("secondaryColor")
+}
+
