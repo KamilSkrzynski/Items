e99ac29012592652d3fc0097749333985d51fa43
KamilSkrzynski <kamilskrzynski95@gmail.com>
2021-04-17 17:07:07 +0200

    TermsPrivacyView

---
+++ Items/Views/TermsPrivacyView/TermsPrivacyView.swift
@@ -0,0 +1,20 @@
+//
+//  TermsPrivacyView.swift
+//  Items
+//
+//  Created by Kamil Skrzyński on 17/04/2021.
+//
+
+import SwiftUI
+
+struct TermsPrivacyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TermsPrivacyView_Previews: PreviewProvider {
+    static var previews: some View {
+        TermsPrivacyView()
+    }
+}
